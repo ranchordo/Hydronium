b54c718e0f0517d0390f116b07f4d96137c8c8a8
Ray <ranchordo@gmail.com>
2022-03-06 10:33:56 -0700

    Test commit (also incl info about SecretConfig)

--- SecretConfig.h
+++ SecretConfig.h
@@ -1,6 +1,12 @@
 #ifndef SECRETCONFIG
 #define SECRETCONFIG
 
+/*
+This file is for secret configuration that shouldn't be committed to the repo.
+To make sure that git doesn't commit this file, either stage the changes, or
+run "git update-index --skip-worktree ./SecretConfig.h" in a terminal.
+*/
+
 #define ENV_WIFI_SSID "WIFI SSID HERE"
 #define ENV_WIFI_PWD "WIFI PASSWORD HERE"
 
